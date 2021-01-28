567e3e668fdd680187e9a6b1e17860177c2b9a9a
NelsonGrimes22 <NelonPGrimes@gmail.com>
2021-01-27 23:21:26 -0500

    fixed api key showing in github

---
+++ CRUD_YouTubeReviews/db/dbKey.js
@@ -0,0 +1,9 @@
+const uri = "mongodb+srv://NelsonG:Nelly!23@cluster0.fxsw4.mongodb.net/Reviews?retryWrites=true&w=majority";	
+
+function getDB(){	
+    return uri;	
+}	
+
+module.exports = {	
+    getDB	
+}; 
